95183280d045d63e7bf45592b8d1682509805d08
beamm536 <beamm023@gmail.com>
2025-01-24 21:14:53 +0100

    estructura archivos para la utilización de room

--- app/src/main/java/com/appclass/app/room/FacturasDatabase.kt
+++ app/src/main/java/com/appclass/app/room/FacturasDatabase.kt
@@ -2,6 +2,7 @@ package com.appclass.app.room
 
 import androidx.room.RoomDatabase
 
+//CONEXIN CON LA BASE DE DATOS
 abstract class FacturasDatabase: RoomDatabase() {
-    abstract fun facturasDao(): FacturasDatabaseDao
+    abstract fun facturasDao(): FacturasDao
 }
--- app/src/main/java/com/appclass/app/room/FacturasDatabaseDao.kt
+++
@@ -1,4 +0,0 @@
-package com.appclass.app.room
-
-interface FacturasDatabaseDao {
-}
