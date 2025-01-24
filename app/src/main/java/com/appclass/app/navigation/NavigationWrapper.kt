d12d2d88dfbab34728a64c409238e23a4c958d21
beamm536 <beamm023@gmail.com>
2025-01-24 20:56:37 +0100

    finalizacion de la creacion de las vistas para los formularios
    > incorporada la navegacion de la segunda pantalla
    > creado el viewmodel de la segunda pantalla

--- app/src/main/java/com/appclass/app/navigation/NavigationWrapper.kt
+++ app/src/main/java/com/appclass/app/navigation/NavigationWrapper.kt
@@ -8,6 +8,8 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.appclass.app.ui.screens.formEntrada.FormularioEntrada
+import com.appclass.app.ui.screens.formEntrada.FormularioEntrada2
+import com.appclass.app.ui.screens.formEntrada.FormularioEntrada2ViewModel
 import com.appclass.app.ui.screens.formEntrada.FormularioEntradaViewModel
 
 @Composable
@@ -15,11 +17,14 @@ fun NavigationWrapper(navController: NavHostController) {
 
     //val navController = rememberNavController()
     val formularioEntradaViewModel: FormularioEntradaViewModel = viewModel()
+    val formularioEntrada2ViewModel: FormularioEntrada2ViewModel = viewModel()
 
-    NavHost(navController = navController, startDestination = AppScreens.FormularioEntrada.ruta){
+    NavHost(navController = navController, startDestination = AppScreens.FormularioEntrada2.ruta){
         composable(AppScreens.FormularioEntrada.ruta) {
             FormularioEntrada(viewModel = formularioEntradaViewModel)
-
+        }
+        composable(AppScreens.FormularioEntrada2.ruta) {
+            FormularioEntrada2(viewModel = formularioEntrada2ViewModel)
         }
     }
 }
